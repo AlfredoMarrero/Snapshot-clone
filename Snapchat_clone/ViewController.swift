49f05768573609a62018225e09a5a278807b0a5e
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-03-16 14:38:32 -0400

    Initial Commit

---
+++ Snapchat_clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat_clone
+//
+//  Created by Alfredo M. on 3/16/17.
+//  Copyright © 2017 Alfredo M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
